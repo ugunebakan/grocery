0c4c0697e902b7346178cb78d463909bc44ade4a
Umut Günebakan <Umut.Gunebakan@netent.com>
2021-10-01 09:59:34 +0200

    parameterize dsn

--- app/models/setup.go
+++ app/models/setup.go
@@ -1,19 +1,28 @@
 package models
 
 import (
-	"gorm.io/gorm"
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDataBase() {
-  dsn := "host=localhost user=admumugun dbname=grocery2 port=5432"
+  hostname := os.Getenv("DATABASE_HOST")
+  user := os.Getenv("DATABASE_USER")
+  password := os.Getenv("DATABASE_PASSWORD")
+  databaseName := os.Getenv("DATABASE_NAME")
+  port := os.Getenv("DATABASE_PORT")
+
+  dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", hostname, user, databaseName, port, password)
+  fmt.Print(dsn)
   database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 	QueryFields: true,
   })
 
-  //database, err := gorm.Open("sqlite3", "test.db")
 
   if err != nil {
     panic("Failed to connect to database!")
